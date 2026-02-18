7b2089d3b4d7fd32fc3e1e03307b445117c75268
ipsracastle-byte <ipsracastle@gmail.com>
2026-02-17 22:02:21 -0600

    haciendo referencia a mi mac

--- datos_mi_pc.py
+++ datos_mi_pc.py
@@ -1,5 +1,5 @@
 import os # importar la librería os
 
 cpu_num = os.cpu_count() # ~~ener~~ obtener el número de CPU en el sistema
-print(f"Mi computadora tiene {cpu_num} núcleos de CPU") # imprimir el número de CPU
+print(f"Mi computadora tiene {cpu_num} núcleos de CPU en mi MAC") # imprimir el número de CPU
 
